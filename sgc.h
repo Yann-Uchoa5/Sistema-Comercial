37803c1887e9ee20b055f311e7e66e561f933295
Yan Uchôa <128929472+Yann-Uchoa5@users.noreply.github.com>
2023-11-12 15:09:05 -0300

    Create sgc.h

---
+++ sgc.h
@@ -0,0 +1,20 @@
+#ifndef sgc_h
+
+#define sgc_h
+
+//Funções para os usuários normais.
+
+void FazerPedido();
+//realizar pedido
+void ExibirListaProdutos();
+//exibir a lista dos produtos
+void AdicaoProduto(); 
+//adicionar algum produto
+void ExibirPedidosUsuarios();
+//exibir pedidos do usuario
+void AdicionarUsuario();
+//adc algum usuario
+void ExcluirUsuario();
+//excluir algum usuario
+void ExcluirPedidoComunicacao();
+#endif
